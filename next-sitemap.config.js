f8cc05711ae49b08540cfc0dfdd108a2148697eb
Milliax <wei@sivir.pw>
2023-06-11 01:45:03 +0800

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,43 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || `https://${process.env.HOST}`,
-    // generateRobotsTxt: true, // (optional)
-    // ...other options
+    transform: async (config, path) => {
+        if (isSecurityPath(path)) {
+            return null
+        }
+
+        if (isMainPage(path)) {
+            return {
+                loc: path,
+                changefreq: 'weekly',
+                priority: 0.9,
+                lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+                alternateRefs: config.alternateRefs ?? [],
+            }
+        }
+
+        // default settings
+
+        return {
+            loc: path,
+            changefreq: config.changefreq,
+            priority: 0.7,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+            alternateRefs: config.alternateRefs ?? [],
+        }
+    }
+}
+
+const isSecurityPath = (page) => {
+    return false
+}
+
+const isMainPage = (page) => {
+    const url = new URL(page);
+    switch (url.pathname) {
+        case "/":
+        case "/about":
+        case "/":
+            return true;
+    }
+    return false;
 }
