418ebfcef1b7ef1e844946e3abef500fef99a24e
milliax <wei@sivir.pw>
2023-08-04 19:06:40 +0800

    calendar good

--- app/(union)/booking/page.tsx
+++ app/(union)/booking/page.tsx
@@ -2,11 +2,17 @@
 import { Switch } from '@headlessui/react'
 import { Section } from '../../../components/Section'
 import { SimpleLayout } from '../../../components/SimpleLayout'
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import clsx from 'clsx'
 import CalendarMonth from '../../../components/calendar/Month'
 
-function SpeakingSection({ children, ...props }: { children: React.ReactNode, title: string }) {
+interface DateTime {
+    year?: number,
+    month?: number,
+    day?: number,
+}
+
+function SpeakingSection({ children, ...props }: { children: React.ReactNode, title: string | Array<string> }) {
     return (
         <Section {...props}>
             <div className="space-y-16">{children}</div>
@@ -16,6 +22,8 @@ function SpeakingSection({ children, ...props }: { children: React.ReactNode, ti
 
 export default function RoomRenting() {
     const [calendarType, setCalendarType] = useState<boolean>(false);
+    const [reserve, setReserve] = useState<boolean>(false);
+    const [dateSelected, setDateSelected] = useState<DateTime>({});
     const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
     return (
@@ -24,7 +32,7 @@ export default function RoomRenting() {
             intro="系學會空間出借"
         >
             <div className="space-y-8">
-                <SpeakingSection title="MOJO DOJO CASA HOUSE">
+                <SpeakingSection title={["MOJO DOJO", "CASA HOUSE"]}>
                     <OnOffSwitch
                         enable={calendarType}
                         setEnable={setCalendarType}
@@ -33,9 +41,36 @@ export default function RoomRenting() {
                     {calendarType ?
                         <CalendarDetailedView />
                         :
-                        <CalendarSeperate />}
+                        <CalendarSeperate
+                            dateSelected={dateSelected}
+                            setDateSelected={setDateSelected}
+                            reserve={reserve}
+                            setReserve={setReserve}
+                        />}
                 </SpeakingSection>
             </div>
+            {reserve && <div
+                className='w-screen h-screen z-50 bg-black fixed top-0 left-0 bg-opacity-70 flex flex-row justify-center items-center '
+                onClick={() => { setReserve(false) }}
+            >
+                <div className="bg-white rounded-lg max-w-3xl w-full z-50 px-5 py-3"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}>
+                    <div>
+
+                    </div>
+                    <div>
+                        選擇日期
+                    </div>
+                    <div>
+                        選擇開始時間
+                    </div>
+                    <div>
+                        預計租借時長
+                    </div>
+                </div>
+            </div>}
         </SimpleLayout>
     )
 }
@@ -48,15 +83,46 @@ function CalendarDetailedView() {
     )
 }
 
-function CalendarSeperate() {
+function CalendarSeperate({
+    reserve,
+    setReserve,
+    dateSelected,
+    setDateSelected,
+}: {
+    reserve: boolean,
+    setReserve: Dispatch<SetStateAction<boolean>>,
+    dateSelected: DateTime,
+    setDateSelected: Dispatch<SetStateAction<DateTime>>,
+}) {
     const today = new Date();
     const [selectedMonth, setSelectedMonth] = useState(today.getMonth());
     const [selectedDay, setSelectedDay] = useState<number>(today.getDate());
     const [hoverDate, setHoverDate] = useState<number>(NaN);
     const [selectedYear, setSelectedYear] = useState<number>(today.getFullYear());
-    const [daysWithEvent, setDaysWithEvent] = useState([1, 2, 4, 8, 9, 13, 16, 22]);
-    const [reserve, setReserve] = useState<boolean>(false);
+    const [daysWithEvent, setDaysWithEvent] = useState<Array<number>>([]);
+    // const [reserve, setReserve] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(true);
+
+    const fetchAPI = async () => {
+        setLoading(true)
+        setDaysWithEvent([])
+        setTimeout(() => {
+            setDaysWithEvent([1, 2, 4, 8, 9, 13, 16, 21]);
+            setLoading(false)
+        }, 1000)
+    }
+
+    useEffect(() => {
+        fetchAPI();
+    }, [selectedMonth, selectedYear])
+
+    useEffect(() => {
+        setDateSelected({
+            year: selectedYear,
+            month: selectedMonth,
+            day: selectedDay,
+        })
+    }, [selectedDay, selectedMonth, selectedYear])
 
     const toggleMonth = (val: 1 | -1) => {
         const month = selectedMonth;
@@ -109,6 +175,7 @@ function OnOffSwitch(
     return (
         <div className="flex flex-row space-x-5">
             <Switch
+                id={`${text}-switch`}
                 className={clsx(
                     "relative inline-flex h-6 w-11 items-center rounded-full",
                     enable ? "bg-blue-600" : "bg-green-600"
