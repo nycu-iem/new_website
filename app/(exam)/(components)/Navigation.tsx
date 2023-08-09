a6228baacf50cb14cd54625c8daf64cc3aa8f5d8
milliax <wei@sivir.pw>
2023-08-10 02:08:46 +0800

    exam page viewable

--- app/(exam)/(components)/Navigation.tsx
+++ app/(exam)/(components)/Navigation.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
@@ -11,11 +11,12 @@ import { useIsInsideMobileNavigation } from './MobileNavigation'
 // import { useSectionStore } from './SectionProvider'
 import { Tag } from './Tag'
 import { remToPx } from '../../../lib/remToPx'
+import { FirstLayerOfPost } from '../notion_api'
 
-function useInitialValue(value, condition = true) {
-    let initialValue = useRef(value).current
-    return condition ? initialValue : value
-}
+// function useInitialValue(value, condition = true) {
+//     let initialValue = useRef(value).current
+//     return condition ? initialValue : value
+// }
 
 function TopLevelNavItem({
     href,
@@ -26,8 +27,7 @@ function TopLevelNavItem({
 }) {
     return (
         <li className="md:hidden">
-            <Link
-                href={href}
+            <Link href={href}
                 className="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
             >
                 {children}
@@ -36,10 +36,21 @@ function TopLevelNavItem({
     )
 }
 
-function NavLink({ href, tag, active, isAnchorLink = false, children }) {
+function NavLink({
+    href,
+    tag,
+    active = false,
+    isAnchorLink = false,
+    children
+}: {
+    href: string,
+    tag?: string,
+    active?: boolean,
+    isAnchorLink?: boolean,
+    children: React.ReactNode,
+}) {
     return (
-        <Link
-            href={href}
+        <Link href={href}
             aria-current={active ? 'page' : undefined}
             className={clsx(
                 'flex justify-between gap-2 py-1 pr-3 text-sm transition',
@@ -59,51 +70,68 @@ function NavLink({ href, tag, active, isAnchorLink = false, children }) {
     )
 }
 
-// function VisibleSectionHighlight({ group, pathname }) {
-//     let [sections, visibleSections] = useInitialValue(
-//         [
-//             useSectionStore((s) => s.sections),
-//             useSectionStore((s) => s.visibleSections),
-//         ],
-//         useIsInsideMobileNavigation()
-//     )
+function VisibleSectionHighlight({
+    group,
+    pathname,
+    sections
+}: {
+    group: FirstLayerOfPost,
+    pathname: string,
+    sections: Array<FirstLayerOfPost>
+}) {
+    // let [sections, visibleSections] = useInitialValue(
+    //     [
+    //         useSectionStore((s) => s.sections),
+    //         useSectionStore((s) => s.visibleSections),
+    //     ],
+    //     useIsInsideMobileNavigation()
+    // )
 
-//     let isPresent = useIsPresent()
-//     let firstVisibleSectionIndex = Math.max(
-//         0,
-//         [{ id: '_top' }, ...sections].findIndex(
-//             (section) => section.id === visibleSections[0]
-//         )
-//     )
-//     let itemHeight = remToPx(2)
-//     let height = isPresent
-//         ? Math.max(1, visibleSections.length) * itemHeight
-//         : itemHeight
-//     let top =
-//         group.links.findIndex((link) => link.href === pathname) * itemHeight +
-//         firstVisibleSectionIndex * itemHeight
+    const visibleSections = sections.map((section) => ({
+        ...section,
+    }))
 
-//     return (
-//         <motion.div
-//             layout
-//             initial={{ opacity: 0 }}
-//             animate={{ opacity: 1, transition: { delay: 0.2 } }}
-//             exit={{ opacity: 0 }}
-//             className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
-//             style={{ borderRadius: 8, height, top }}
-//         />
-//     )
-// }
-
-function ActivePageMarker({ group, pathname }) {
-    let itemHeight = remToPx(2)
-    let offset = remToPx(0.25)
-    let activePageIndex = group.links.findIndex((link) => link.href === pathname)
-    let top = offset + activePageIndex * itemHeight
+    const isPresent = useIsPresent()
+    const firstVisibleSectionIndex = Math.max(
+        0,
+        [{ title: '_top' }, ...sections].findIndex(
+            (section) => section.title === visibleSections[0].title
+        )
+    )
+    const itemHeight = remToPx(2)
+    const height = isPresent
+        ? Math.max(1, visibleSections.length) * itemHeight
+        : itemHeight
+    const top =
+        group.classes.findIndex((course) => `${group.link}/${course.link}` === pathname) * itemHeight +
+        firstVisibleSectionIndex * itemHeight
 
     return (
         <motion.div
             layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: 0.2 } }}
+            exit={{ opacity: 0 }}
+            className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
+            style={{ borderRadius: 8, height, top }}
+        />
+    )
+}
+
+function ActivePageMarker({
+    group,
+    pathname
+}: {
+    pathname: string,
+    group: FirstLayerOfPost,
+}) {
+    const itemHeight = remToPx(2)
+    const offset = remToPx(0.25)
+    const activePageIndex = group.classes.findIndex((course) => `${group.link}/${course.link}` === pathname)
+    const top = offset + activePageIndex * itemHeight
+
+    return (
+        <motion.div layout
             className="absolute left-2 h-6 w-px bg-emerald-500"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { delay: 0.2 } }}
@@ -113,20 +141,28 @@ function ActivePageMarker({ group, pathname }) {
     )
 }
 
-function NavigationGroup({ group, className }) {
+function NavigationGroup({
+    group,
+    className,
+    sections,
+}: {
+    className: React.ReactNode,
+    group: FirstLayerOfPost,
+    sections: Array<FirstLayerOfPost>
+}) {
     // If this is the mobile navigation then we always render the initial
     // state, so that the state does not change during the close animation.
     // The state will still update when we re-open (re-render) the navigation.
-    let isInsideMobileNavigation = useIsInsideMobileNavigation()
+    const isInsideMobileNavigation = useIsInsideMobileNavigation()
     // let [pathname, sections] = useInitialValue(
     //     [usePathname(), useSectionStore((s) => s.sections)],
     //     isInsideMobileNavigation
     // )
 
-    const pathname = ""
-
-    let isActiveGroup =
-        group.links.findIndex((link) => link.href === pathname) !== -1
+    const pathname = usePathname()
+    // console.log(`${group.link}/${group.classes[0]?.link}`, pathname)
+    const isActiveGroup = group.classes.findIndex((post) => encodeURI(`${group.link}/${post.link}`) === pathname) !== -1
+    // console.log(isActiveGroup)
 
     return (
         <li className={clsx('relative mt-6', className)}>
@@ -139,11 +175,10 @@ function NavigationGroup({ group, className }) {
             <div className="relative mt-3 pl-2">
                 <AnimatePresence initial={!isInsideMobileNavigation}>
                     {isActiveGroup && (
-                        <VisibleSectionHighlight group={group} pathname={pathname} />
+                        <VisibleSectionHighlight group={group} pathname={pathname} sections={sections} />
                     )}
                 </AnimatePresence>
-                <motion.div
-                    layout
+                <motion.div layout
                     className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"
                 />
                 <AnimatePresence initial={false}>
@@ -151,14 +186,15 @@ function NavigationGroup({ group, className }) {
                         <ActivePageMarker group={group} pathname={pathname} />
                     )}
                 </AnimatePresence>
+
                 <ul role="list" className="border-l border-transparent">
-                    {group.links.map((link) => (
-                        <motion.li key={link.href} layout="position" className="relative">
-                            <NavLink href={link.href} active={link.href === pathname}>
-                                {link.title}
+                    {group.classes.map((course) => (
+                        <motion.li key={course.link} layout="position" className="relative">
+                            <NavLink href={`${group.link}/${course.link}`} active={`${group.link}/${course.link}` === pathname}>
+                                {course.title}
                             </NavLink>
                             <AnimatePresence mode="popLayout" initial={false}>
-                                {link.href === pathname && sections.length > 0 && (
+                                {`${group.link}/${course.link}` === pathname && group.classes.length > 0 && (
                                     <motion.ul
                                         role="list"
                                         initial={{ opacity: 0 }}
@@ -171,14 +207,13 @@ function NavigationGroup({ group, className }) {
                                             transition: { duration: 0.15 },
                                         }}
                                     >
-                                        {sections.map((section) => (
-                                            <li key={section.id}>
-                                                <NavLink
-                                                    href={`${link.href}#${section.id}`}
-                                                    tag={section.tag}
+                                        {course.sections.map((section) => (
+                                            <li key={section}>
+                                                <NavLink href={`${group.link}/${course.link}#${section}`}
+                                                    tag={section}
                                                     isAnchorLink
                                                 >
-                                                    {section.title}
+                                                    {section}
                                                 </NavLink>
                                             </li>
                                         ))}
@@ -193,42 +228,24 @@ function NavigationGroup({ group, className }) {
     )
 }
 
-export const navigation = [
-    {
-        title: 'Guides',
-        links: [
-            { title: 'Introduction', href: '/' },
-            { title: 'Quickstart', href: '/quickstart' },
-            { title: 'SDKs', href: '/sdks' },
-            { title: 'Authentication', href: '/authentication' },
-            { title: 'Pagination', href: '/pagination' },
-            { title: 'Errors', href: '/errors' },
-            { title: 'Webhooks', href: '/webhooks' },
-        ],
-    },
-    {
-        title: 'Resources',
-        links: [
-            { title: 'Contacts', href: '/contacts' },
-            { title: 'Conversations', href: '/conversations' },
-            { title: 'Messages', href: '/messages' },
-            { title: 'Groups', href: '/groups' },
-            { title: 'Attachments', href: '/attachments' },
-        ],
-    },
-]
-
-export function Navigation(props) {
+export function Navigation({
+    sections,
+    ...props
+}: {
+    className?: string,
+    sections: Array<FirstLayerOfPost>
+}) {
     return (
         <nav {...props}>
-            <ul role="list">
-                <TopLevelNavItem href="/">API</TopLevelNavItem>
-                <TopLevelNavItem href="#">Documentation</TopLevelNavItem>
-                <TopLevelNavItem href="#">Support</TopLevelNavItem>
-                {navigation.map((group, groupIndex) => (
+            <ul role="list" className='relative'>
+                <TopLevelNavItem href="/articles">文章</TopLevelNavItem>
+                <TopLevelNavItem href="/activities">活動</TopLevelNavItem>
+                <TopLevelNavItem href="/booking">系窩租借</TopLevelNavItem>
+                {sections.map((group, groupIndex) => (
                     <NavigationGroup
                         key={group.title}
                         group={group}
+                        sections={sections}
                         className={groupIndex === 0 && 'md:mt-0'}
                     />
                 ))}
