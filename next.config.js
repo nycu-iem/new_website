dc5611b7c9fa9518c1f67b238c5583f534a77d4e
milliax <maxchengboy@gmail.com>
2025-03-05 11:33:49 +0800

    update to auth5

---
+++ next.config.js
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+// import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+// const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+
+// if (process.env.NODE_ENV === 'development') {
+//     await setupDevPlatform();
+// }
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "s3.us-west-2.amazonaws.com"
+            }, {
+                protocol: "https",
+                hostname: "**"
+            }
+        ]
+    },
+    // config.module.rules.push()
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+            test: /\.node/,
+            use: 'raw-loader',
+        });
+
+        return config;
+    },
+    // experimental: {
+    //     serverActions: true,
+    // },
+
+    // webpack: (config) => { config.externals.push({ sharp: 'commonjs sharp', canvas: 'commonjs canvas' }) return config }
+}
+
+module.exports = nextConfig
